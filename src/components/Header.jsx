47a4b7dabb3cf3639762b867b9d7cbf0301ea032
diego vallejo <vallejodiego@live.com>
2024-06-28 16:21:38 -0300

    premerge

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -11,85 +11,59 @@ const Header = () => {
     const dispatch = useDispatch()
     const navigate = useNavigate()
     const role = useSelector(store => store.roleReducer.role)
-    
+
     const toggleMenu = () => {
         setIsOpen(!isOpen);
     };
 
-<<<<<<< HEAD
-    useEffect(() => {
-        if (isOpen) {
-            console.log('open menu');
-        } else {
-            console.log('close menu');
-        }
-    }, [isOpen]);
-
-    const handleClick = () => {
-        console.log('Button clicked');
-    };
-=======
-    const handleLogout = () => {                                       
+    const handleLogout = () => {
         dispatch(eraseRole())
         dispatch(logout())
-        navigate("/")    
+        navigate("/")
     }
->>>>>>> 1069233cb6176defeb2216c61cec2c6296a73453
 
     return (
         <section>
-    <header className="bg-[#73383E] h-[100px] md:h-[150px] flex flex-row justify-between  items-center px-5 relative z-50">
-        {/* IMAGEN LOGO */}
-        <img src="/assets/logo-2.png" className="w-[100px] md:w-[150px] " alt="logo-home" />
-
-<<<<<<< HEAD
-            <button onClick={() => console.log('Button clicked')} style={{ padding: '10px', backgroundColor: 'blue', color: 'white' }}>
-                Click me
-            </button>
-
-            <div onClick={handleClick} style={{ padding: '10px', backgroundColor: 'blue', color: 'white' }}>
-                Click me
-            </div>
+            <header className="bg-[#73383E] h-[100px] md:h-[150px] flex flex-row justify-between  items-center px-5 relative z-50">
+                {/* IMAGEN LOGO */}
+                <img src="/assets/logo-2.png" className="w-[100px] md:w-[150px] " alt="logo-home" />
 
 
-=======
+                {!isAuthenticated ? <>
+                    <button className=' bg-[#5e2a30] w-[80px] flex justify-center px-4 py-2  rounded-lg text-white  shadow-[0_3px_10px_rgb(0,0,0,0.2)]' >
+                        <Anchor href="/login" text="Login" />
+                    </button>
+                    <button className=' bg-[#5e2a30] px-4 w-[80px] flex justify-center py-2 rounded-lg text-white  shadow-[0_3px_10px_rgb(0,0,0,0.2)]'>
+                        <Anchor href="/registerClient" text="Register" />
+                    </button>
+                </> :
+                    <>
+                        <button onClick={handleLogout} className=' bg-[#5e2a30] px-4 py-2 rounded-lg text-white  shadow-[0_3px_10px_rgb(0,0,0,0.2)]'>
+                            <Anchor href="/" text="Logout" />
+                        </button>
+                        {role === "client" && <NavLink to="/carrito">
+                            <img src="./assets/cart.png" alt="cart" className="w-8 h-8" />
+                        </NavLink>}
 
-        {!isAuthenticated ? <>
-            <button className=' bg-[#5e2a30] w-[80px] flex justify-center px-4 py-2 rounded-lg text-white shadow-[0_3px_10px_rgb(0,0,0,0.2)]' > 
-                <Anchor href="/login" text="Login" />
-            </button>
-            <button className=' bg-[#5e2a30] px-4 w-[80px] flex justify-center py-2 rounded-lg text-white shadow-[0_3px_10px_rgb(0,0,0,0.2)]'>
-                <Anchor href="/registerClient" text="Register" />
-            </button>
-        </> : 
-        <>
-            <button onClick={handleLogout} className=' bg-[#5e2a30] px-4 py-2 rounded-lg text-white  shadow-[0_3px_10px_rgb(0,0,0,0.2)]'>
-                <Anchor href="/" text="Logout" />
-            </button>
-            {role === "client" && <NavLink to="/carrito">
-                <img src="./assets/cart.png" alt="cart" className="w-8 h-8" />
-            </NavLink>}
-            
-        </>
-        }
->>>>>>> 1069233cb6176defeb2216c61cec2c6296a73453
+                    </>
+                }
 
-        {/* Menú de navegación */}
-        <nav className={`absolute md:static lg:static top-[84px] lg:mr-10 left-0 right-0 bg-[#73383E] md:bg-transparent lg:bg-transparent transition-max-height duration-500 ease-in-out overflow-hidden ${isOpen ? 'max-h-screen' : 'max-h-0'} z-50 md:max-h-full lg:max-h-full md:flex md:items-center lg:flex lg:items-center`}>
-            <div className="flex flex-col md:flex-row lg:flex-row items-center md:text-xl md:gap-5 text-white my-5 md:my-0 lg:my-0">
-                <Anchor href="/" text="Home" />
-                <Anchor href="/wines" text="Wines" />
-                <Anchor href="/contact" text="Contact" />
-                {role === "admin" ? <Anchor href="/admin" text="Admin" /> : role==="client" && <Anchor href="/client" text="Client" />}
-            </div>
-        </nav>
+                {/* Menú de navegación */}
+                <nav className={`absolute md:static lg:static top-[84px] lg:mr-10 left-0 right-0 bg-[#73383E] md:bg-transparent lg:bg-transparent transition-max-height duration-500 ease-in-out overflow-hidden ${isOpen ? 'max-h-screen' : 'max-h-0'} z-50 md:max-h-full lg:max-h-full md:flex md:items-center lg:flex lg:items-center`}>
+                    <div className="flex flex-col md:flex-row lg:flex-row items-center md:text-xl md:gap-5 text-white my-5 md:my-0 lg:my-0">
+                        <Anchor href="/" text="Home" />
+                        <Anchor href="/wines" text="Wines" />
+                        <Anchor href="/contact" text="Contact" />
+                        {role === "admin" ? <Anchor href="/admin" text="Admin" /> : role === "client" && <Anchor href="/client" text="Client" />}
+                    </div>
+                </nav>
 
-        {/* Botón de menú hamburguesa para vistas móviles */}
-        <button className="md:hidden lg:hidden text-white focus:outline-none" onClick={toggleMenu}>
-            <img src="./assets/menu.png" className="w-10" alt="menu" />
-        </button>
-    </header>
-</section>
+                {/* Botón de menú hamburguesa para vistas móviles */}
+                <button className="md:hidden lg:hidden text-white focus:outline-none" onClick={toggleMenu}>
+                    <img src="./assets/menu.png" className="w-10" alt="menu" />
+                </button>
+            </header>
+        </section>
     );
 };
 
